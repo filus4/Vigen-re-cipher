2360c3ac5608944c13ab8230b2737ac6ae3f4ea3
marcel <filej1025@gmail.com>
2020-10-28 12:12:45 +0100

    added function to decode with known key

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ vector<int> get_ascii_key(string file_name) {
     return ascii_vkey;
 }
 
-int main() {
+void encode() {
     ifstream input;
     ofstream output;
 
@@ -43,42 +43,75 @@ int main() {
 
     int key_length = ascii_vkey.size();
 
-    input.open("input.txt");
-    output.open("output.txt");
+    input.open("encode_input.txt");
+    output.open("encode_output.txt");
 
     char current_char;
-    int current_index;
-    current_index = 0;
+    int current_index = 0;
     int current_char_in_ascii;
     int new_position_in_alphabet;
 
     while (input >> noskipws >> current_char) {       
         if (isalpha(current_char)) {
             current_char_in_ascii = (int)toupper(current_char);
-
             int shift_index = current_index % key_length;
 
             new_position_in_alphabet = ((current_char_in_ascii + ascii_vkey[shift_index]) - 65 ) % 26;
             char shifted_char = char(new_position_in_alphabet + 65);
 
             output << shifted_char;
-
             current_index += 1;
-        } else if (!isalpha(current_char)) {
+        } else {
             output << current_char;
         } 
     }
-
     current_index = 0;
-    
     
     input.close();
     output.close();
+}
+
+void decode_with_key(){
+    ifstream input;
+    ofstream output;
+
+    vector<int> ascii_vkey = get_ascii_key("key.txt");
+    int key_length = ascii_vkey.size();
+
+    input.open("encode_output.txt");
+    output.open("decode_output.txt");
+
+    char current_char;
+    int current_index = 0;
+    int current_char_in_ascii;
 
 
-    print_number(ascii_vkey);
-    cout << endl;
-    cout << "Size of the given vector is: " << ascii_vkey.size();
+    while (input >> noskipws >> current_char) {
+        if (isalpha(current_char)) {
+            int shift_index = current_index % key_length;
+            char shifted_char = ((int)toupper(current_char) - ascii_vkey[shift_index]);
+            if (shifted_char < 65) {
+                output << char(shifted_char + 26);
+                current_index += 1;
+            } else {
+                output << char(shifted_char);
+                current_index += 1;
+            }
+        } else {
+            output << current_char;
+        }
+    }
+    current_index = 0;
+    
+    input.close();
+    output.close();
+}
+
+int main() {
+    
+    encode();
+
+    decode_with_key();
 
     return 0;
 }
